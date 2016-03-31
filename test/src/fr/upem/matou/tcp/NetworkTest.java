d2bda9ec15af7554529e9819d0f40c520367385b
DartzPower <spakpower@hotmail.fr>
2016-03-31 19:29:17 +0200

    Mission pour Pape !

--- test/src/fr/upem/matou/tcp/NetworkTest.java
+++ test/src/fr/upem/matou/tcp/NetworkTest.java
@@ -28,14 +28,33 @@ public class NetworkTest {
 			e.printStackTrace();
 		}
 	};
-	
+
 	@Test
 	public void testConnection() throws InterruptedException {
 		Thread server = new Thread(serverMain);
 		Thread client = new Thread(clientMain);
 		server.start();
 		client.start();
-		
+
+		/*
+		 * TODO Pape :
+		 * 
+		 * Trouver un moyen de rediriger le flux d'entrée/sortie standard du thread "client" vers des fichiers de test
+		 * OU de lire/écrire des string dynamiquement.
+		 * 
+		 * Le but est de pouvoir écrire du code comme ça dans le client :
+		 * 
+		 * ClientInputRedirection.input("foo"); // Ecrit "foo" sur l'entrée standard de ShellInterface
+		 * String output = ClientInputRedirection.output(); // Lit l'entrée standard de ShellInterface
+		 * 
+		 * OU BIEN
+		 * 
+		 * Path inputPath, outputPath; // On défini 2 fichiers de test (un en lecture, un en écriture)
+		 * ClientInputRedirection.setInput(inputPath); // On redirige l'entrée standard de ShellInterface
+		 * ClientInputRedirection.setOutput(outputPath); // On redirige la sortie standard de ShellInterface
+		 * 
+		 */
+
 		client.join();
 		server.interrupt();
 	}
