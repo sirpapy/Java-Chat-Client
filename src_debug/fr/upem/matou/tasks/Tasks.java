f3f7d5ecc49dbaf85c4dd3de867d55552296b135
DartzPower <spakpower@hotmail.fr>
2016-05-01 16:53:10 +0200

    Tasks

--- src_debug/fr/upem/matou/tasks/Tasks.java
+++ src_debug/fr/upem/matou/tasks/Tasks.java
@@ -1,14 +1,6 @@
 package fr.upem.matou.tasks;
 
-// TEMP : Classe temporaire où figurent les TASKS générales au projet
-
 @SuppressWarnings("javadoc")
 public class Tasks {
-
-	// TODO : Vérifier qu'il n'y a pas d'entiers/chaines hardcodées
-	
-	// TODO : Tests unitaires
-	
-	// TODO : Ecrire le rapport
-	
+	// FIXME : Générer les JAR
 }
